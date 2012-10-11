eccbf0c4c3311c49d160451cda88945326bd0a83
Jeff Kolb <jeff.kolb@cern.ch>
2012-10-11 15:03:50 +0000

    added TriggerFiller (but leave it disabled for now)

--- test/makeNtuple_cfg.py
+++ test/makeNtuple_cfg.py
@@ -1,39 +1,46 @@
 import FWCore.ParameterSet.Config as cms
 import copy
 import sys
-import os
 
 
 # === Give values to some basic parameters === #
 maxEvents	= -1
-#maxEvents	= 100
-reportEvery	= 1000
+reportEvery	= 100
 tauMaxEta	= 9
 tauMinPt	= 0
 
-
-# === Collection postfix for running on PF2APT === #
-postfix		= ''
-#postfix		= 'PFlow'
-
+# collection postfix for running on PF2APT
+postfix = ''
+# postfix = 'PFlow'
 
 # === Parse external arguments === #
 import FWCore.ParameterSet.VarParsing as VarParsing
 options = VarParsing.VarParsing("analysis")
-options.register("analysisType",
-       #"coll",		# default value
-       #"mc",		# default value
-       "signal",	# default value
-       VarParsing.VarParsing.multiplicity.singleton, # singleton or list
-       VarParsing.VarParsing.varType.string,         # string, int, or float
-       "is MC or is Data?"
-       )   
+# analysisType parameter form: 
+# 
+# <era>_<type>_<lepton flavor>
+#
+# <era> = Fall11,Spring12,2011A,2011B,2012X
+# <type> = MC_sig, MC_bg, data_PR, data_RR
+# <lepton flaor> = muon, electron
+#
+# Examples:
+# Fall11_MC_sig_muon
+# 2011B_data_PR_electron
+# Summer12_MC_bg_electron
+# 2012B_data_PR_muon
+options.register ('analysisType', # 
+                  'Fall11_MC_sig_muon', # default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string )
 options.maxEvents = maxEvents
-options.outputFile = 'NUT.root'
-#options.inputFiles = 'file:/afs/crc.nd.edu/group/NDCMS/data02/jkolb//TauResults/Run45/TTH_130_skim.root'
-options.inputFiles = '/store/user/jkolb/TTH_HtoAll_M_125_7TeV_pythia6/skimTTHiggsToDiTau_428_v5_TTH_125_xrootd//457a50077bda71f732b6d36b2ce09e9b/ttHiggsToDiTauSkim_112_1_hir.root'
-options.parseArguments() # get and parse the command line arguments (or from Xcrab.cfg)
+options.outputFile = 'ntuple.root'
+options.inputFiles = 'file:/store/user/jkolb/TTH_HtoTauTau_M_125_7TeV_FullSim_Pythia6_v2/skimTTHiggsToDiTau_428_v8_TTH_125_FullSim/25a6c8a18b2b0964299388fc37b7979d/ttHiggsToDiTauSkim_100_1_NwK.root'
+options.parseArguments() # get and parse the command line arguments 
 
+if options.analysisType.find('muon') == -1 and options.analysisType.find('electron') == -1:
+    print 'analysisType must contain "muon" or "electron"'
+    exit(1)
 
 # === Print some basic info about the job setup === #
 print ''
@@ -41,27 +48,83 @@ print '	========================================='
 print '		Ntuple Making Job'
 print '	========================================='
 print ''
-print '		Analysis type...%s' % options.analysisType
-print '		Max events......%d' % options.maxEvents
-print '		Report every....%d' % reportEvery
-print ''
-#print '		Max tau |eta|...%d' % tauMaxEta
-#print '		Min tau pT......%d' % tauMinPt
+print '		Sample ID......%s' % options.analysisType
+print '		Max events.....%d' % options.maxEvents
+print '		Report every...%d' % reportEvery
 print ''
 print '	========================================='
 print ''
 
-# === Set up genparticles collection based on analysis type === # 
-if (options.analysisType == "signal") or (options.analysisType == "mc"):
-    inputForGenParticles	= 'genParticles'
-    inputForGenJets			= 'selectedPatJets:genJets:'
-elif (options.analysisType == "coll"):
-    inputForGenParticles	= ''
-    inputForGenJets			= ''
-else:
-    sys.exit("Analysis type not understood")
+# === Set up triggers and GEN collections based on analysis type === # 
+if options.analysisType.find('MC') != -1:
+    inputForGenParticles = 'genParticles'
+    inputForGenJets     = 'selectedPatJets:genJets:'
+    triggerConditions = (
+        'HLT_IsoMu24_v*',
+        'HLT_IsoMu24_eta2p1_v*'
+    )
+    if options.analysisType.find('electron') != -1:
+        triggerConditions = (
+            'HLT_Ele25_CaloIdVT_TrkIdT_TriCentralJet30_v*',
+            'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TriCentralJet30_v*',
+            'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TriCentralPFJet30_v*'
+        )
+
+if options.analysisType.find('data') != -1:
+    inputForGenParticles = ''
+    inputForGenJets         = ''
+    triggerConditions = (
+        'HLT_IsoMu24_v*',
+        'HLT_IsoMu24_eta2p1_v*'
+    )
+    if options.analysisType.find('electron') != -1:
+        triggerConditions = (
+            'HLT_Ele25_CaloIdVT_TrkIdT_TriCentralJet30_v*',
+            'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TriCentralJet30_v*',
+            'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TriCentralPFJet30_v*'
+        )
 
 
+# === make analysis-specific selections for skims, fillers, etc. === #
+SkimTriggerRequirements	= cms.vstring()
+        #	'ttHiggsElectronSkim',
+        #   'ttHiggsMuonSkim',
+        #	'ttElectronHiggsToElecTauSkim',
+        #	'ttMuonHiggsToElecTauSkim',
+        #	'ttElectronHiggsToMuTauSkim',
+        #	'ttMuonHiggsToMuTauSkim',
+        #	'ttElectronHiggsToTauTauSkim',
+        #	'ttMuonHiggsToTauTauSkim',
+        #	'ttHiggsToElecTauSkim',
+        #	'ttHiggsToMuTauSkim',
+        #   'ttHiggsToTauTauSkim',
+
+if (options.analysisType.find('sig') != -1 ) and (options.analysisType.find('muon') != -1):
+    SkimTriggerRequirements = cms.vstring('ttMuonHiggsToTauTauSkim')
+if (options.analysisType.find('sig') != -1 ) and (options.analysisType.find('electron') != -1):
+    SkimTriggerRequirements = cms.vstring('ttElectronHiggsToTauTauSkim')
+if (options.analysisType.find('bg') != -1 ) and (options.analysisType.find('muon') != -1):
+    SkimTriggerRequirements = cms.vstring('ttHiggsMuonSkim')
+if (options.analysisType.find('bg') != -1 ) and (options.analysisType.find('electron') != -1):
+    SkimTriggerRequirements = cms.vstring('ttHiggsElectronSkim')
+if (options.analysisType.find('data') != -1 ) and (options.analysisType.find('muon') != -1):
+    SkimTriggerRequirements = cms.vstring('ttHiggsMuonSkim')
+if (options.analysisType.find('data') != -1 ) and (options.analysisType.find('electron') != -1):
+    SkimTriggerRequirements = cms.vstring('ttHiggsElectronSkim')
+
+NtupleFillers = cms.untracked.vstring(
+        'Event',
+        'GenLevel',
+        'GenTau',
+        'GenJet',
+        'Tau',
+        'Electron',
+        'Muon',
+        'Jet',
+        'DitauMuon',
+        'DitauElectron',
+        #'Trigger' # not in use
+)
 
 # === Python process === #
 process = cms.Process('TTbarHTauTau')
@@ -79,76 +142,43 @@ process.source = cms.Source("PoolSource",
 )
 process.TFileService = cms.Service("TFileService", fileName = cms.string(options.outputFile) )
 
-
-# === Override input files with all the files in the following directory === #
-dir='/afs/crc.nd.edu/group/NDCMS/data02/jkolb/TauResults/TTHtoTauTau/ntuples/MuonPlusJets/skim_v8/'
-process.source.fileNames = cms.vstring()
-for file in os.listdir(dir):
-    process.source.fileNames.extend(cms.vstring(dir+file))
-
-
-
 # === Collision data trigger requirements === #
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 process.hltFilter = hlt.triggerResultsFilter.clone(
-    hltResults = cms.InputTag('TriggerResults::HLT'),
-    triggerConditions = (
-							'HLT_IsoMu24_eta2p1_v*',
-    ),
-    l1tResults = '',
-    throw = True
+        hltResults = cms.InputTag('TriggerResults::HLT'),
+        triggerConditions = cms.vstring(triggerConditions),
+        l1tResults = '',
+        throw = False
 )
+
+analysisTypeForNtuplizer = 'coll'
+if options.analysisType.find('sig') != -1 :
+    analysisTypeForNtuplizer = 'signal'
+if options.analysisType.find('bg') != -1 :
+    analysisTypeForNtuplizer = 'mc'
 
 
 # === Define and setup main module === #
 process.makeNtuple = cms.EDAnalyzer('Ntuplizer',
 
 	# === Analysis setup === #
-	AnalysisType						= cms.string(options.analysisType),					# Type of analysis, i.e. sample (coll, mc, signal)
-    TreeName							= cms.untracked.string('tree'),
+	AnalysisType						= cms.string(analysisTypeForNtuplizer),				
+    TreeName							= cms.untracked.string('TTbarHTauTau'),
+
+	# === HL Trigger === # (not in use)
+    HLTriggerSource		    			= cms.InputTag("TriggerResults::HLT"),
 
 	# === Skim Trigger === #
     ApplySkimTriggerRequirements		= cms.bool(True),
     SkimTriggerSource					= cms.InputTag("TriggerResults","","skimTTHiggsToDiTau"),
-    SkimTriggerRequirements				= cms.vstring(
-													#	'ttHiggsElectronSkim',
-													#	'ttHiggsMuonSkim',
-													#	'ttElectronHiggsToElecTauSkim',
-													#	'ttMuonHiggsToElecTauSkim',
-													#	'ttElectronHiggsToMuTauSkim',
-                                                    #	'ttMuonHiggsToMuTauSkim',
-													#	'ttElectronHiggsToTauTauSkim',
-													#	'ttMuonHiggsToTauTauSkim',
-														'ttHiggsToTauTauSkim',
-														),
-
-	# === Trigger === #
-    #ApplyTriggerRequirements			= cms.bool(True),
-    #TriggerSource						= cms.InputTag("TriggerResults","","HLT"),
-    #TriggerRequirements					= cms.vstring(
-	#													'HLT_IsoMu30_v1',
-    #													),
+    SkimTriggerRequirements				= SkimTriggerRequirements,
 
 	# === Which branches to fill? === #
-	NtupleFillers						= cms.untracked.vstring(
-																'Event',
-																#'GenLevel',
-																#'GenTau',
-																#'GenJet',
-																#'Tau',
-																#'Electron',
-																#'Muon',
-																#'Jet',
-																#'Ditau',
-																#'DitauElectron',
-																'DitauMuon',
-																#'DitauMuonCheck',
-																#'NilsGenMatcher',
-																),
+	NtupleFillers						= NtupleFillers,
 
 	# === Input collections === #
     GenParticleSource					= cms.untracked.InputTag(inputForGenParticles),
-    GenJetSource						= cms.untracked.InputTag(inputForGenJets),
+    GenJetSource                        = cms.untracked.InputTag(inputForGenJets),
     RecoTauSource						= cms.InputTag('selectedPatTaus'+postfix),
     RecoMuonSource						= cms.InputTag('selectedPatMuons'+postfix),
     RecoElectronSource					= cms.InputTag('selectedPatElectrons'+postfix),
@@ -162,9 +192,9 @@ process.makeNtuple = cms.EDAnalyzer('Ntuplizer',
     RecoTauRequireDMF                   = cms.bool(False),
 
 	# === Jet stuff === #
-    RecoJetMinEt						= cms.double(15.0),
+    RecoJetMinEt						= cms.double(30.0),
     RecoJetMinAbsEta					= cms.double(0.0),
-    RecoJetMaxAbsEta					= cms.double(2.5),
+    RecoJetMaxAbsEta					= cms.double(2.4),
     JetAntiMatchingDeltaR               = cms.double(0.25),
 	CSVlooseWP							= cms.double(0.244),
 	CSVmediumWP							= cms.double(0.679),
@@ -174,12 +204,12 @@ process.makeNtuple = cms.EDAnalyzer('Ntuplizer',
 )
 
 # === Run sequence === # 
-if (options.analysisType == "coll"):
+if options.analysisType.find('data') != -1:
     process.p = cms.Path( process.hltFilter + process.makeNtuple )
 else:
     process.p = cms.Path( process.makeNtuple )
 
-
 # === Write-out all python configuration parameter information === #
-pythonDump = open("dumpedPython.py", "write"); print >> pythonDump,  process.dumpPython()
+#pythonDump = open("dumpedPython.py", "write")
+#print >> pythonDump,  process.dumpPython()
 
