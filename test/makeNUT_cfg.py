a366f42b8de96adf6dbe9cff94f8be4582194f97
Nil Valls <nil@nilvalls.com>
2012-11-13 13:08:50 +0000

    *** empty log message ***

--- test/makeNUT_cfg.py
+++ test/makeNUT_cfg.py
@@ -6,8 +6,12 @@ import sys
 # === Give values to some basic parameters === #
 maxEvents	= -1
 reportEvery	= 100
+maxEvents	= 100
+reportEvery	= 1
 tauMaxEta	= 9
 tauMinPt	= 0
+
+debugLevel	= 0 # 0: no debug, 5: full debug
 
 # collection postfix for running on PF2APT
 postfix = ''
@@ -32,6 +36,7 @@ options = VarParsing.VarParsing("analysis")
 # 2012_X_MC-bg_electron
 # 2012_B_data-PR_muon
 options.register ('analysisType', # 
+				  #'2012_X_data-PR_muon_-11',	# -1		2012A collisions
 				  #'2012_X_MC-bg_muon_-1',	# -1		2012A collisions
 				  #'2012_X_MC-bg_muon_-11',	# -11		2012B collisions
 				  #'2012_X_MC-bg_muon_2500',	# 2500	TTbar
@@ -45,20 +50,24 @@ options.register ('analysisType', #
 				  #'2012_X_MC-bg_muon_2702',	# 2702	ZZ
 				  #'2012_X_MC-bg_muon_2504',	# 2504	sT+W
 				  #'2012_X_MC-bg_muon_2505',	# 2505	sTbar+W	
-				  '2012_X_MC-bg_muon_2600',	# 2600	sT-sCh
+				  #'2012_X_MC-bg_muon_2600',	# 2600	sT-sCh
 				  #'2012_X_MC-bg_muon_2501',	# 2501	sTbar-sCh
 				  #'2012_X_MC-bg_muon_2602',	# 2602	sT-tCh
 				  #'2012_X_MC-bg_muon_2503',	# 2503	sTbar-tCh
-				  #'2012_X_MC-bg_muon_9115',	# 9115	TTH_115_Fast
-				  #'2012_X_MC-bg_muon_9120',	# 9120	TTH_120_Fast
-				  #'2012_X_MC-bg_muon_9125',	# 9125	TTH_125_Fast
+				  #'2012_X_MC-sig_muon_9115',	# 9115	TTH_115_Fast
+				  '2011_X_MC-sig_muon_9120',	# 9120	TTH_120_Fast
+				  #'2012_X_MC-sig_muon_9125',	# 9125	TTH_125_Fast
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.string )
 options.maxEvents = maxEvents
 options.outputFile = 'NUT.root'
 #options.inputFiles = 'file:/store/user/jkolb/TTH_HtoTauTau_M_125_7TeV_FullSim_Pythia6_v2/skimTTHiggsToDiTau_428_v8_TTH_125_FullSim/25a6c8a18b2b0964299388fc37b7979d/ttHiggsToDiTauSkim_100_1_NwK.root'
 #options.inputFiles = 'file:/afs/crc.nd.edu/user/n/nvallsve/CMSSW_5_2_5/src/NtupleMaker/BEANmaker/TTbar_summer12_BEAN.root'
-options.inputFiles = '/store/user/lannon/T_s-channel_TuneZ2star_8TeV-powheg-tauola/Summer12-PU_S7_START52_V9-v1_BEAN_53xOn52x_V01_CV01//74c4602d4f424e29e54ad6a04efd57f1/pat2bean_53x_9_1_1yE.root'
+#options.inputFiles = '/store/user/lannon/T_s-channel_TuneZ2star_8TeV-powheg-tauola/Summer12-PU_S7_START52_V9-v1_BEAN_53xOn52x_V01_CV01//74c4602d4f424e29e54ad6a04efd57f1/pat2bean_53x_9_1_1yE.root'
+#options.inputFiles = '/store/user/lannon/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/skimDilep_Summer12-PU_S7_START52_V9_53xOn52x_V02_CV01_ttjets_unpublished/skimDilep_ttjets_v2_job011.root'
+#options.inputFiles = '/store/user/lwming/TTH_HToAll_M_125_8TeV_FastSim_pythia6/TTH_HToAll_M_125_8TeV_FastSim_pythia6/191b19832235558f2b51f7486e9bfa14/TTH_HToAll_M_125_8TeV_FastSim_v1_117_1_dO5.root'
+#options.inputFiles = '/store/user/lannon/MuEG/skimDilep_53xOn52x_V02_CV01_MuEG_Run2012B/a9860641c79dea2455e3b18f5a7c9285/output_10_1_smT.root'
+options.inputFiles = '/store/user/lannon/TTH_HToAll_M_110_8TeV_FastSim_pythia6/TTH_Fastsim_BEAN_53xOn52x_V01_CV01//1ffa50b9e8fddadad25e9301b4aa6ffb/pat2bean_53x_12_1_bEw.root'
 options.parseArguments() # get and parse the command line arguments 
 
 if options.analysisType.find('muon') == -1 and options.analysisType.find('electron') == -1:
@@ -80,8 +89,8 @@ print ''
 
 # === Set up triggers and GEN collections based on analysis type === # 
 if options.analysisType.find('MC') != -1:
-    inputForGenParticles = 'genParticles'
-    inputForGenJets     = 'selectedPatJets:genJets:'
+    inputForGenParticles = 'BNproducer:MCstatus3:'
+    inputForGenJets     = 'BNproducer:ak5GenJets:'
     triggerConditions = (
         'HLT_IsoMu24_v*',
         'HLT_IsoMu24_eta2p1_v*'
@@ -136,16 +145,16 @@ if (options.analysisType.find('data') != -1 ) and (options.analysisType.find('el
     SkimTriggerRequirements = cms.vstring('ttHiggsElectronSkim')
 
 NtupleFillers = cms.untracked.vstring(
-        'Event',
-        'GenLevel',
-        'GenTau',
-        'GenJet',
-        'Tau',
-        'Electron',
-        'Muon',
-        'Jet',
+#        'Event',
+#        'GenLevel',
+#        'GenTau',
+#        'GenJet',
+#        'Tau',
+#        'Electron',
+#        'Muon',
+#        'Jet',
         'DitauMuon',
-        'DitauElectron',
+#        'DitauElectron',
         #'Trigger' # not in use
 )
 
@@ -170,7 +179,8 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string(options
 # === Collision data trigger requirements === #
 import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
 process.hltFilter = hlt.triggerResultsFilter.clone(
-        hltResults = cms.InputTag('TriggerResults::HLT'),
+        #hltResults = cms.InputTag('TriggerResults::HLT'),
+        hltResults = cms.InputTag(''),
         triggerConditions = cms.vstring(triggerConditions),
         l1tResults = '',
         throw = False
@@ -181,12 +191,13 @@ process.hltFilter = hlt.triggerResultsFilter.clone(
 process.makeNtuple = cms.EDAnalyzer('Ntuplizer',
 
 	# === Analysis setup === #
+	DebugLevel							= cms.uint32(debugLevel),
 	AnalysisType						= cms.string(options.analysisType),				
 	FromBEAN							= cms.bool(True),
     TreeName							= cms.untracked.string('TTbarHTauTau'),
 
 	# === HL Trigger === # (not in use)
-    HLTriggerSource		    			= cms.InputTag("TriggerResults::HLT"),
+    HLTriggerSource		    			= cms.InputTag("BNproducer:HLT"),
 
 	# === Skim Trigger === #
     ApplySkimTriggerRequirements		= cms.bool(True),
@@ -199,13 +210,7 @@ process.makeNtuple = cms.EDAnalyzer('Ntuplizer',
     # === Input collections === #
     GenParticleSource                   = cms.untracked.InputTag(inputForGenParticles),
     GenJetSource                        = cms.untracked.InputTag(inputForGenJets),
-    #RecoVertexSource                   = cms.InputTag('offlinePrimaryVertices'),
     RecoPATMetSource                    = cms.InputTag('patMETs'+postfix),
-    #RecoElectronSource                 = cms.InputTag('selectedPatElectrons'+postfix),
-    #RecoMuonSource                     = cms.InputTag('selectedPatMuons'+postfix),
-    #RecoTauSource                      = cms.InputTag('selectedPatTaus'+postfix),
-    #RecoJetSource                      = cms.InputTag('selectedPatJets'+postfix+'::skimTTHiggsToDiTau'),
-    #RecoPFMetSource                    = cms.InputTag('patMETs'+postfix),
     RecoVertexSource                    = cms.InputTag('BNproducer:offlinePrimaryVertices'),
     RecoElectronSource                  = cms.InputTag('BNproducer:selectedPatElectronsPFlow'+postfix),
     RecoMuonSource                      = cms.InputTag('BNproducer:selectedPatMuonsPFlow'+postfix),
@@ -232,6 +237,7 @@ process.makeNtuple = cms.EDAnalyzer('Ntuplizer',
 # === Run sequence === # 
 if (options.analysisType.find('data') != -1):
     process.p = cms.Path( process.hltFilter + process.makeNtuple )
+    #process.p = cms.Path( process.makeNtuple )
 else:
     process.p = cms.Path( process.makeNtuple )
 
