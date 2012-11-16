72092c36699418b66d20ec7137530d91045830fb
Jeff Kolb <jeff.kolb@cern.ch>
2012-11-16 16:52:59 +0000

    moved from TTHTauTau/Skimming; updated for 53X 2012

---
+++ python/globalTagMap_cfi.py
@@ -0,0 +1,18 @@
+globalTagMap = {}
+# year_era_type
+globalTagMap['2011_X_MC-sig'] = 'START42_V14B'
+globalTagMap['2011_X_MC-bg'] = 'START42_V14B'
+globalTagMap['2011_A_data-PR'] = 'GR_R_42_V25' # don't use PR tags
+globalTagMap['2011_A_data-RR'] = 'GR_R_42_V25'
+globalTagMap['2011_B_data-PR'] = 'GR_R_42_V25' # don't use PR tags
+globalTagMap['2011_B_data-RR'] = 'GR_R_42_V25'
+globalTagMap['2012_X_MC-bg'] = 'START53_V7A'
+globalTagMap['2012_X_MC-sig'] = 'START53_V7A'
+globalTagMap['2012_A_data-PR'] = 'GR_P_V42'
+globalTagMap['2012_A_data-RR'] = 'GR_R_53_V10'
+globalTagMap['2012_B_data-PR'] = 'GR_P_V42'
+globalTagMap['2012_B_data-RR'] = 'GR_R_53_V10'
+globalTagMap['2012_C_data-PR'] = 'GR_P_V42'
+globalTagMap['2012_C_data-RR'] = 'GR_R_53_V10'
+globalTagMap['2012_D_data-PR'] = 'GR_P_V42'
+globalTagMap['2012_D_data-RR'] = 'GR_R_53_V10'
