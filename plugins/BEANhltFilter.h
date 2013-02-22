feaba7d0731cbaea33b9a6daa8185dd7f9d55863
Jeff Kolb <jeff.kolb@cern.ch>
2013-02-22 20:56:30 +0000

    first commit

---
+++ plugins/BEANhltFilter.h
@@ -0,0 +1,46 @@
+// -*- C++ -*-
+//
+// Package:    BEANhltFilter
+// Class:      BEANhltFilter
+// 
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "ProductArea/BNcollections/interface/BNtrigger.h"
+//
+// class declaration
+//
+
+class BEANhltFilter : public edm::EDFilter {
+   public:
+      explicit BEANhltFilter(const edm::ParameterSet&);
+      ~BEANhltFilter();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+   private:
+      virtual void beginJob() ;
+      virtual bool filter(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+      
+      virtual bool beginRun(edm::Run&, edm::EventSetup const&);
+      virtual bool endRun(edm::Run&, edm::EventSetup const&);
+      virtual bool beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+      virtual bool endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+
+      // ----------member data ---------------------------
+
+      std::string hltAcceptPath_;
+};
+
