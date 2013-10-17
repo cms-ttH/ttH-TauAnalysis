50be5d356a6e76e8f35745e73c921a1f876bf2d2
Matthias Wolf <matthias@sushinara.net>
2013-10-17 16:08:41 -0400

    Add more jet properties.

--- interface/JetFiller.h
+++ interface/JetFiller.h
@@ -1,3 +1,4 @@
+// vim: et:sta:sw=4:ts=4
 // Authors: Andres Florez, Alfredo Gurrola, Eduardo Luiggi, Chi Nhan Nguyen, Nitish Dhingra, Nil Valls
 
 #ifndef _JetFiller_h
@@ -29,6 +30,23 @@ class JetFiller : public NtupleFiller {
 		vector<float>		_JetPt;
 		vector<float>		_JetEta;
 		vector<float>		_JetPhi;
+        vector<double> _JetCSV;
+        vector<float> _JetCharge;
+        vector<float> _JetChargePtWeighed;
+        vector<int> _JetPartonId;
+        vector<int> _JetPartonMother0Id;
+        vector<int> _JetPartonMother1Id;
+        vector<int> _JetPartonGrandmother00Id;
+        vector<int> _JetPartonGrandmother01Id;
+        vector<int> _JetPartonGrandmother10Id;
+        vector<int> _JetPartonGrandmother11Id;
+        vector<int> _JetPartonStatus;
+        vector<int> _JetPartonMother0Status;
+        vector<int> _JetPartonMother1Status;
+        vector<int> _JetPartonGrandmother00Status;
+        vector<int> _JetPartonGrandmother01Status;
+        vector<int> _JetPartonGrandmother10Status;
+        vector<int> _JetPartonGrandmother11Status;
 		vector<float>		_combSecVtxBTag;
 		vector<bool>		_combSecVtxLooseBTag;
 		vector<bool>		_combSecVtxMediumBTag;
